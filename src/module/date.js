da463769c403b1ee441efc37a6b3b685dc096101
PASSER-BY <zhenghaole1990@126.com>
2023-07-13 01:24:19 +0800

    updated 模块整理

--- src/module/date.js
+++ src/module/date.js
@@ -6,11 +6,6 @@ import {getZodiac} from './zodiac';
 import {getAnimalYear} from './animal';
 import {getFestivalsBySolar,getFestivalsByLunar,getTermFestivalsBySolar} from './festival';
 
-// 月份
-const monthMap = ['正','二','三','四','五','六','七','八','九','十','冬','腊'];
-// 十位
-const dayMap = ['初一','初二','初三','初四','初五','初六','初七','初八','初九','初十','十一','十二','十三','十四','十五','十六','十七','十八','十九','二十','廿一','廿二','廿三','廿四','廿五','廿六','廿七','廿八','廿九','三十'];
-
 // 获取指定日志下的数据
 export function getDateInfo(timestamp){
     let result = getSolarByTimestamp(timestamp);
@@ -25,8 +20,6 @@ export function getDateInfo(timestamp){
         result['animal'] = getAnimalYear(result['lYear']);
         result['term'] = getTerm(result['sYear'],result['sMonth'],result['sDay']);
         festivals = festivals.concat(getTermFestivalsBySolar(result['sYear'],result['sMonth'],result['sDay']));
-        result['lMonthZH'] = (result['isLeap']?'闰':'')+monthMap[result['lMonth']-1]+'月';
-        result['lDayZH'] = dayMap[result['lDay']-1];
         festivals = festivals.concat(getFestivalsByLunar(result['lYear'],result['lMonth'],result['lDay']));
     }
     festivals = festivals.concat(getFestivalsBySolar(result['sYear'],result['sMonth'],result['sDay']));
--- src/module/lunar.js
+++ src/module/lunar.js
@@ -5,7 +5,11 @@ import {
     minDay,
     maxYear
 } from './data/map';
-
+// 月份
+const monthMap = ['正','二','三','四','五','六','七','八','九','十','冬','腊'];
+// 十位
+const dayMap = ['初一','初二','初三','初四','初五','初六','初七','初八','初九','初十','十一','十二','十三','十四','十五','十六','十七','十八','十九','二十','廿一','廿二','廿三','廿四','廿五','廿六','廿七','廿八','廿九','三十'];
+// 参考时间点
 const startTime = Date.UTC(minYear, minMonth-1, minDay, 0, 0, 0);
 
 // 获取农历年闰月
@@ -110,6 +114,8 @@ export function getLunarByTimestamp(timestamp){
         lYear:lYear,
         lMonth:lMonth,
         lDay:lDay,
-        isLeap:isLeap
+        isLeap:isLeap,
+        lMonthZH:(isLeap?'闰':'')+monthMap[lMonth-1]+'月',
+        lDayZH:dayMap[lDay-1]
     };
 }
