ac911159a1d414f19788f7d48083e156ce12ab18
zhenghaole <89932980@qq.com>
2023-10-23 18:24:36 +0800

    updated 语句简写

--- src/module/tool.js
+++ src/module/tool.js
@@ -1,6 +1,6 @@
 // 格式化日期字符串
 export function getDateString(...param){
-    return Array.from(param).map(function(value){
+    return param.map(function(value){
         return (''+value).padStart(2,'0');
     }).join('-');
 }
