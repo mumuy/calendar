9185e3ee26d85892d884ac6920e310423fbc5862
zhenghaole <89932980@qq.com>
2023-10-27 11:32:57 +0800

    updated 插件调整

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = {
     presets: ['@babel/preset-env'],
     plugins: [
         '@babel/plugin-transform-runtime',
-        '@babel/plugin-proposal-class-properties',
+        '@babel/plugin-transform-class-properties',
         [
             '@babel/plugin-syntax-import-attributes',
             {
