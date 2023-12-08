8a12719cda54fcb408c8a374ccc3812c0bc9ecaf
zhenghaole <89932980@qq.com>
2023-12-08 10:08:57 +0800

    updated 添加语法兼容的垫片

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,14 @@
 module.exports = {
-    presets: ['@babel/preset-env'],
+    presets: [
+        ['@babel/preset-env',{
+            modules:false,
+            useBuiltIns:'usage',
+            corejs: '3.21',
+            targets:{
+                chrome: 89
+            }
+        }]
+    ],
     plugins: [
         '@babel/plugin-transform-runtime',
         '@babel/plugin-transform-class-properties',
