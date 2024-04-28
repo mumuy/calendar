5c671ae3bb29ea406197d2489bfb139d71700e1e
zhenghaole <89932980@qq.com>
2024-04-28 19:08:41 +0800

    updated 构建工具调整

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,6 @@ module.exports = {
     ],
     plugins: [
         '@babel/plugin-transform-runtime',
-        '@babel/plugin-transform-class-properties',
         [
             '@babel/plugin-syntax-import-attributes',
             {
